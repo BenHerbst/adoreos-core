3c3eafd1fbb89a560ecfdce0fdd878602d526337
Ben Herbst <83538916+BenHerbst@users.noreply.github.com>
2022-01-08 23:25:42 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-namespace myNamespace {
-    export function myFunction(myParameter: number) {
-
-    }
-
-    export class MyClass {
-        myField: number;
-
-        myMethod() {
-
-        }
-    }
-
-    export enum MyEnum {
-        MyEnumMember
-    }
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+//% groups=['LED matrix', 'Control flow', 'others']
+namespace basic {
+export function showNumbers(v: number, interval: number = 150): void
+{ }
 }
