99b3345604a9b80c2aa1ef585c6bd69dd202982d
Ben Herbst <83538916+BenHerbst@users.noreply.github.com>
2022-01-08 23:26:14 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  */
 //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
 //% groups=['LED matrix', 'Control flow', 'others']
-namespace basic {
+namespace adoreos {
 export function showNumbers(v: number, interval: number = 150): void
 { }
 }
